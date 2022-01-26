0b16f5d0777086e2e9b053d77b1a1b752d0eea97
gsham <gsham@12cm.co.kr>
2022-01-26 15:11:31 +0900

    오더 까지 완료

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 // 구현체 하나가 있을 경우에는 Impl 이라고 관례상 한다.
 public class MemberServiceImpl  implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,16 +1,24 @@
-package hello.core.core.member;
+package hello.core.member;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     public void join() {
