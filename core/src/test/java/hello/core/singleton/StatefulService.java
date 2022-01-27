5b0a8cb04a1e9c1a01e73f7e806f7bcafa1bb50e
gsham <gsham@12cm.co.kr>
2022-01-27 13:58:40 +0900

    스프링 컨테이너 빈들을 의존성 주입할때 싱글톤 보장

--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -2,15 +2,16 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price; // 상태를 유지하는 필드
 
-    public void order(String name, int price) {
+    // private int price; // 상태를 유지하는 필드 싱글톤으로 사용되어지면 안되기 떄문에 사용X
+
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
 
-        this.price = price;
-    }
-
-    public int getPrice() {
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 }
--- core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -1,6 +1,5 @@
 package hello.core.singleton;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -9,21 +8,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StatefulServiceTest {
 
-
     @Test
     void statefulServiceSingleton() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
         StatefulService statefulService = ac.getBean(StatefulService.class);
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
-        statefulService.order("userA", 10000);
-        statefulService2.order("userB", 20000);
+        int price = statefulService.order("userA", 10000);
+        int price2 = statefulService2.order("userB", 20000);
 
         // ThreadA: 사용자A 주문 금액 조회
-        int price = statefulService.getPrice();
         System.out.println("price = " + price);
 
-        assertThat(statefulService.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
@@ -34,7 +30,5 @@ public class StatefulServiceTest {
         }
 
     }
-
-
 
 }
