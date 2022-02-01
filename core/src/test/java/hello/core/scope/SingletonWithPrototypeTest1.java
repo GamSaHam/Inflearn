fc36d1eb0eaafd9a4de560ce0ee7be1ae5369ae1
gsham <gsham@12cm.co.kr>
2022-02-01 13:12:13 +0900

    prototype 관련 라이브러리 추가

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -47,8 +48,12 @@ public class SingletonWithPrototypeTest1 {
     static class ClientBean {
 //        private final PrototypeBean prototypeBean; // 생성시점에 주입
 
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
 //        public ClientBean(PrototypeBean prototypeBean) {
 //            this.prototypeBean = prototypeBean;
@@ -56,7 +61,9 @@ public class SingletonWithPrototypeTest1 {
 
         public int logic(){
 
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+
             prototypeBean.addCount();
 
             // cmd + opt + n
