3009615c17ccb3b05cb2d9e1541d4f00738d7a8d
gsham <gsham@12cm.co.kr>
2022-03-06 21:33:11 +0900

    retry 추가

---
+++ aop/src/main/java/hello/aop/exam/aop/RetryAspect.java
@@ -0,0 +1,20 @@
+package hello.aop.exam.aop;
+
+import hello.aop.exam.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class RetryAspect {
+
+
+    @Around("@annotation(retry)")
+    public Object doRetry(ProceedingJoinPoint joinPoint, Retry retry) {
+
+    }
+
+
+}
