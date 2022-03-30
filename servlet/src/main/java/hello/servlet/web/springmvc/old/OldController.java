955dcd995f4849e8b6a5b19395260c753de335f2
gsham <gsham@12cm.co.kr>
2022-03-30 16:40:40 +0900

    spring mvc controller

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -14,7 +14,7 @@ public class OldController implements Controller {
 
         System.out.println("OldController.handleRequest");
 
-        return null;
+        return new ModelAndView("new-form");
     }
 
 
