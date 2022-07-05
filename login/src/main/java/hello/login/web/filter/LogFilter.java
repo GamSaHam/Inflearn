4e501b2e499bddf7a40e76c916b5920ecddd411c
gsham <gsham@12cm.co.kr>
2022-07-05 17:19:55 +0900

    servlet interceptor 완료

--- login/src/main/java/hello/login/web/filter/LogFilter.java
+++ login/src/main/java/hello/login/web/filter/LogFilter.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 public class LogFilter implements Filter {
@@ -18,7 +19,18 @@ public class LogFilter implements Filter {
         log.info("log filter doFilter");
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
 
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);
+        }catch (Exception e) {
+            throw e;
+        }finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
 
 
 
